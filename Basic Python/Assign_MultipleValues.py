7238c8820cfdc35ed705a548761e826ddb2918e7
ruhul <ra.01403288711@gmail.com>
2023-05-16 16:03:38 +0600

    Assigning multiple values

---
+++ Basic Python/Assign_MultipleValues.py
@@ -0,0 +1,24 @@
+name,email,contact = "Ruhul Amin","r@gmail.com","01322-352864"
+
+print(name)
+print(email)
+print(contact)
+
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+#Unpack Collections
+
+print("=================================")
+array = ['name','email','contact']
+
+x,y,z = array
+
+print(x)
+print(y)
+print(z)
+print("=================================")
+
