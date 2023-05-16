f9cf20748dc5384252801eec0d72fc1f41ae1ae5
ruhul <ra.01403288711@gmail.com>
2023-05-16 15:35:35 +0600

    Python Variable

---
+++ Basic Python/Python_variable.py
@@ -0,0 +1,19 @@
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+y = 12
+x = 2*y
+print(x)
+
+#type casting
+
+x = str(12*3)
+print(type(x))
+x = float(x)
+print(type(x))
+x = int(x)
+print(type(x))
+print(x)
+
+#Get the Type
+
+print(type(x))
