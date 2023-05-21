4d176dfa5c1f2d73b8ee43d3aa6dd00e909b7035
ruhul <ra.01403288711@gmail.com>
2023-05-21 11:38:41 +0600

    Slicing String

---
+++ Basic Python/Python_String.py
@@ -0,0 +1,21 @@
+
+a = "Hello, World!"
+print(a[1])
+
+for x in "banana":
+    print(x)
+
+x = "This is a good"
+print(len(x))
+
+x = "This is a good text"
+print("text" in x)
+
+x = "This is my text"
+if 'text' in x:print("yes! present!")
+
+x = "This is an expensive text!"
+if a not in x:print("YES! a is absent!")
+
+
+
