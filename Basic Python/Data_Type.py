b6ad0ff341a3227050c279c483b9fb403ee4920b
ruhul <ra.01403288711@gmail.com>
2023-05-21 10:59:22 +0600

    Data Type

---
+++ Basic Python/Data_Type.py
@@ -0,0 +1,38 @@
+x = "Hellow World"
+print(type(x))
+x = 20
+print(type(x))
+x = 20.5
+print(type(x))
+x = 1j
+print(type(x))
+x = ['apple', 'banana', 'carrot']
+print(type(x))
+x = ('apple', 'banana', 'carrot')
+print(type(x))
+x = range(6)
+print(type(x))
+x = {"name":"Ruhul","age":28,"contact":"01322-352864"}
+print(type(x))
+x = {"apple","mango","banana"}
+print(type(x))
+x = frozenset({"apple","mango","banana"})
+print(type(x))
+x = True
+print(type(x))
+x = False
+print(type(x))
+x = b"Hello"
+print(type(x))
+x = r'[A-Z]'
+print(type(x))
+x = {"name" : "John", "age" : 36}
+x = bytearray(5)
+x = None
+x = memoryview(bytes(5))
+print(x)
+
+
+
+
+
