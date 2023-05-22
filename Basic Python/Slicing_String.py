e87ed47437fd91d523f41603dde0522a536a088e
ruhul <ra.01403288711@gmail.com>
2023-05-22 12:55:14 +0600

    Slicing String

---
+++ Basic Python/Slicing_String.py
@@ -0,0 +1,13 @@
+
+#slicing string
+str = "Hello Bangladesh"
+print(str[2:4])
+print(str[5:])
+print(str[1:])
+b = "Hello, World!"
+print(b[-5:-2])
+
+
+str2 = "Hi_This_is_My_Name"
+ans = str2.split("_")
+print(ans)
