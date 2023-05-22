ae8d1ce69ad7fed568e8ab24c6d62e2b93cc495c
ruhul <ra.01403288711@gmail.com>
2023-05-22 13:16:39 +0600

    format string

---
+++ Basic Python/Format_String.py
@@ -0,0 +1,8 @@
+
+name = "Ruhul Amin"
+email = "ruhul.cs.dev@gmail.com"
+contact = "01322-352864"
+
+text = "Hi This is {}. My email is {} and my contact is {}"
+print(text.format(name,email,contact))
+
