b5c34a4810df9d24ac9c669dd023f36e271f8c2f
ruhul <ra.01403288711@gmail.com>
2023-05-25 16:57:10 +0600

    Class and object

---
+++ Basic Python/PythonClass/class.py
@@ -0,0 +1,10 @@
+class myX:
+    x = 'Ruhul'
+    y = 'Riya moni'
+print(myX)
+
+obj = myX()
+print(obj.x,obj.y)
+
+
+
---
+++ Basic Python/PythonClass/init.py
@@ -0,0 +1,29 @@
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+
+obj = Person("Ruhul",28)
+print(obj.name)
+
+class Animal:
+    def __init__(self,animalName,type,age):
+        self.animalName = animalName
+        self.type = type
+        self.age = age
+
+    def printInfo(self):
+        text = "The Animal Name is {}.And it is {} type Animal and its about {} years old"
+        print(text.format(self.animalName,self.type,self.age))
+
+
+a1 = Animal("Lion","Ferocious",4)
+print(a1.printInfo())
+
+# class definitions cannot be empty, but if you for some reason have a class definition with no content,
+# put in the pass statement to avoid getting an error.
+
+class Veachel:pass
+class Modern:pass
+class Fish:pass
